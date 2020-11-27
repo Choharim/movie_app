7e834a25d77670ecd20bcf18ef879c27e965e1c5
Choharim <jhl8586@naver.com>
2020-11-27 17:49:51 +0900

    Add unique Key Props

--- src/App.js
+++ src/App.js
@@ -11,26 +11,26 @@ function Food({name,picture}){
 
 const foodILike = [
   {
+    id:1,
     name:"kimchi",
     image:"https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi.jpg"
   },
   {
+    id:2,
     name:"samgyupsal",
     image:"https://cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/IZOXUQBP4QVT4ONUPAIDA6347M.jpg"
   },
   {
+    id:3,
     name:"bibipbap",
     image:"https://files.bonif.co.kr/upload/cmdt/BF102_pic_LjMg3BTB.jpg"
   }];
 
-function renderFood(dish){
-  return <Food name={dish.name} picture={dish.image}></Food>
-}
 
 function App() {
   return (
     <div className="App">
-    {foodILike.map(renderFood)}
+    {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image}></Food>))}
     </div>
   );
 }
