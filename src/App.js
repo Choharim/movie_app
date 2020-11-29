a9e7c27f7791025f7ad4f27615ad4e8638caf9ea
Choharim <jhl8586@naver.com>
2020-11-29 16:57:12 +0900

    Add setState

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component{
+  state = {
+    count:0
+  };//component의 데이터를 바꾸기 위해 사용하는 것
+
+  add = () => {
+    this.setState({count: this.state.count +1});
+  };
+  minus = () => {
+    this.setState({count: this.state.count -1});
+  };
+
   render(){
-    return <h1>I am a class Component</h1>;
+  return (
+  <div>
+    <h1>The number is {this.state.count}</h1>
+    <button onClick={this.add}>Add</button>
+    <button onClick={this.minus}>Minus</button> 
+  </div>
+  );
   }
-}
+}//class component는 render함수를 자동적으로 실행시킴
 
 
 export default App;
