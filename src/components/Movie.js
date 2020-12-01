b0c85d4cc1855cb043d6025c8fa497e95f64a1f6
Choharim <jhl8586@naver.com>
2020-12-01 18:47:05 +0900

    Add Navigation

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import "./Movie.css";
 
-function Movie({year,title,summary,poster,genres}){
+function Movie({id,year,title,summary,poster,genres}){
 return (
-<div className="movie">
-  <img src={poster} alt={title} title={title}></img>
-  <div className="movie_data">
-    <h3 className="movie_title">{title}</h3>
-    <h5 className="movie_year">{year}</h5>
-    <ul className="movie_genres">
-    {genres.map((genre,index) => <li className="genres_genre" key={index}>{genre}</li>)}
-    </ul>
-    <p className="movie_summary">{summary.slice(0, 150)}...</p>
-  </div>
-</div>);
+  <Link to={{
+    pathname:`/movie/:${id}`,
+    state:{
+      year,
+      title,
+      summary,
+      poster,
+      genres
+    } }}>
+    <div className="movie">
+      <img src={poster} alt={title} title={title}></img>
+      <div className="movie_data">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <ul className="movie_genres">
+        {genres.map((genre,index) => <li className="genres_genre" key={index}>{genre}</li>)}
+        </ul>
+        <p className="movie_summary">{summary.slice(0, 150)}...</p>
+      </div>
+    </div>
+  </Link>
+);
 }
 
 Movie.propTypes = {
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function About(){
-  return <span>About this pages</span>
+  return (
+    <div className="about-container">
+      <span>
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."" 
+      </span>
+      <span>1984</span>
+    </div>
+  );
 }
 
 export default About;
