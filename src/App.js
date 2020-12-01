f475c076ab54cea798abfe509c946019b4a77757
Choharim <jhl8586@naver.com>
2020-12-01 17:21:10 +0900

    Add Home,About

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import {HashRouter, Route} from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
-class App extends React.Component{
-  state = {
-    movies:[],
-    isLoading:true
-  };
-
-  getMovies = async() => {
-    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
-    this.setState({movies, isLoading:false}); // {movies:movies, isloading:false}
-  };
-
-  componentDidMount(){
-   this.getMovies();
-  }
-
-  render(){
-    const {isLoading, movies} = this.state;  //this.state는 state object를 가져온 것
-
-    return (
-      <section className="container">
-        {isLoading ? (
-        <div className="loader"><span className="loader_text">Loading...</span></div>
-         ) : (
-           <div className="movies">
-           {movies.map(movie => (
-          <Movie
-           key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}
-          />))} 
-           </div>
-            ) 
-         }
-      </section>
-    );
-  }
-}//class component는 render함수를 자동적으로 실행시킴, class component사용하는 이유는 state사용하려고
-
+function App(){
+  return <HashRouter>
+    <Route path="/" component={Home}></Route>
+    <Route path="/about" component={About}></Route>
+  </HashRouter>
+}
 
 export default App;
